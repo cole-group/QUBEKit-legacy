764bd727e2b07f7f2bb9fcab8ed7abdc9f6bbc4a
Josh Horton <J.T.Horton2@newcastle.ac.uk>
2018-08-02 15:06:00 +0100

    new_install

---
+++ QuBe_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Script will install QuBeKit and append it to your bashrc path"
+
+echo "make sure you are in your home directory"
+pwd
+echo "install here[y/n] " 
+
+read answer
+
+if [ $answer == 'y' ]; then
+   mkdir QuBeKit
+   mv bin/ matlab/ onetep/ README.md QuBeKit
+   cd QuBeKit/bin
+   chmod +x QuBeKit.py 
+   cd ../../
+   path=`pwd`
+   cd
+   echo "" >> .bashrc
+   echo "#added by QuBeKit" >> .bashrc
+   echo "export PATH=\"$path/QuBeKit/bin:\$PATH\"" >> .bashrc
+   echo "export QuBeKit=\"$path/QuBeKit/\"" >> .bashrc
+   echo "QuBeKit installed"
+   
+else
+ echo "exiting"
+ 
+fi
+    
+
+
+
